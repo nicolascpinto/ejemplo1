954276d6688f40ff41ddff409fc092db91550166
nicolascpinto <npintoh@udistrital.edu.co>
2023-04-29 07:49:13 -0500

    Primer código :+1:

---
+++ app.py
@@ -0,0 +1,6 @@
+#importar backend
+from backend.areas import *
+print("Esto es una pruebaaa")
+
+areaCirculo(100)
+areaCuadrado(100)
---
+++ backend/areas.py
@@ -0,0 +1,11 @@
+"""
+Acá vamos a calcular diferentes áreas
+"""
+
+import math
+
+def areaCirculo(radio):
+    print(math.pi * radio **2)
+    
+def areaCuadrado(lado):
+    print(lado * lado)
